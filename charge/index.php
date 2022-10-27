5b8e2f913f0233ebbe0d51131ea383dcc51fad38
AdeeeW <adewijayanto2@gmail.com>
2022-10-27 13:48:30 +0700

    update file charge

--- charge/index.php
+++ charge/index.php
@@ -1,47 +1,58 @@
 <?php
-{
-    "payment_type": "credit_card",
-    "transaction_details": {
-      "order_id": "C17550",
-      "gross_amount": 145000
+curl -X POST \
+  https://api.sandbox.midtrans.com/v2/charge \
+  -H 'Accept: application/json'\
+  -H 'Authorization: Basic <YOUR SERVER KEY ENCODED in Base64>' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "payment_type": "credit_card",
+  "transaction_details": {
+    "order_id": "order102",
+    "gross_amount": 13000
+  },
+  "credit_card": {
+    "token_id": "<token_id from Get Card Token Step>",
+    "authentication": true,
+  },
+  "item_details": [
+    {
+      "id": "a01",
+      "price": 7000,
+      "quantity": 1,
+      "name": "Apple"
     },
-    "credit_card": {
-      "token_id": "SB-Mid-client-2dnztsuJA_eWo8OQ"
+    {
+      "id": "b02",
+      "price": 3000,
+      "quantity": 2,
+      "name": "Orange"
+    }
+  ],
+  "customer_details": {
+    "first_name": "Budi",
+    "last_name": "Susanto",
+    "email": "budisusanto@example.com",
+    "phone": "+628123456789",
+    "billing_address": {
+      "first_name": "Budi",
+      "last_name": "Susanto",
+      "email": "budisusanto@example.com",
+      "phone": "08123456789",
+      "address": "Sudirman No.12",
+      "city": "Jakarta",
+      "postal_code": "12190",
+      "country_code": "IDN"
     },
-    "item_details": [{
-        "id": "a1",
-        "price": 145000,
-        "quantity": 2,
-        "name": "Apel",
-        "brand": "Fuji Apple",
-        "category": "Fruit",
-        "merchant_name": "Fruit-store"
-      }],
-      "customer_details": {
-        "first_name": "BUDI",
-        "last_name": "UTOMO",
-        "email": "test@midtrans.com",
-        "phone": "+628123456",
-        "billing_address": {
-          "first_name": "BUDI",
-          "last_name": "UTOMO",
-          "email": "test@midtrans.com",
-          "phone": "081 2233 44-55",
-          "address": "Sudirman",
-          "city": "Jakarta",
-          "postal_code": "12190",
-          "country_code": "IDN"
-        },
-        "shipping_address": {
-          "first_name": "BUDI",
-          "last_name": "UTOMO",
-          "email": "test@midtrans.com",
-          "phone": "0 8128-75 7-9338",
-          "address": "Sudirman",
-          "city": "Jakarta",
-          "postal_code": "12190",
-          "country_code": "IDN"
-        }
-      }
+    "shipping_address": {
+      "first_name": "Budi",
+      "last_name": "Susanto",
+      "email": "budisusanto@example.com",
+      "phone": "0812345678910",
+      "address": "Sudirman",
+      "city": "Jakarta",
+      "postal_code": "12190",
+      "country_code": "IDN"
+    }
   }
+}'
 ?>
