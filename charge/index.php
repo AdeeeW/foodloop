404db1befd48ded684fa57003986e8c9b428bca1
AdeeeW <adewijayanto2@gmail.com>
2022-10-27 11:23:29 +0700

    ubah index charge

--- charge/index.php
+++ charge/index.php
@@ -1,24 +1,23 @@
 <?php
-curl -X POST -d '{  
-   "transaction_details":{  
-      "order_id":"mobile-12345",
-      "gross_amount":280000
-   },
-   "item_details":[  
-      {  
-         "id":"A01",
-         "price":280000,
-         "quantity":1,
-         "name":"Mie Ayam Komplit"
-      }
-   ],
-   "customer_details":[  
-      {  
-         "email":"tester@example.com",
-         "first_name":"Budi",
-         "last_name":"Khannedy",
-         "phone":"628112341234"
-      }
-   ]
-}' "https://foodloop.herokuapp.com/charge/index.php/"
+\Midtrans\Config::$serverKey = 'SB-Mid-client-2dnztsuJA_eWo8OQ';
+ 
+$params = array(
+    'transaction_details' => array(
+        'order_id' => rand(),
+        'gross_amount' => 10000,
+    ),
+    'payment_type' => 'credit_card',
+    'credit_card'  => array(
+        'token_id'      => $_POST['token_id'],
+        'authentication'=> true,
+    ),
+    'customer_details' => array(
+        'first_name' => 'budi',
+        'last_name' => 'pratama',
+        'email' => 'budi.pra@example.com',
+        'phone' => '08111222333',
+    ),
+);
+ 
+$response = \Midtrans\CoreApi::charge($params);
 ?>
