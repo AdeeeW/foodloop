fe28344d0dcf7b29506e68f2baefec3b8f77f01e
AdeeeW <adewijayanto2@gmail.com>
2022-10-27 11:50:03 +0700

    ubah file index charge

---
+++ charge/index.php
@@ -0,0 +1,47 @@
+<?php
+{
+    "payment_type": "credit_card",
+    "transaction_details": {
+      "order_id": "C17550",
+      "gross_amount": 145000
+    },
+    "credit_card": {
+      "token_id": "SB-Mid-client-2dnztsuJA_eWo8OQ"
+    },
+    "item_details": [{
+        "id": "a1",
+        "price": 145000,
+        "quantity": 2,
+        "name": "Apel",
+        "brand": "Fuji Apple",
+        "category": "Fruit",
+        "merchant_name": "Fruit-store"
+      }],
+      "customer_details": {
+        "first_name": "BUDI",
+        "last_name": "UTOMO",
+        "email": "test@midtrans.com",
+        "phone": "+628123456",
+        "billing_address": {
+          "first_name": "BUDI",
+          "last_name": "UTOMO",
+          "email": "test@midtrans.com",
+          "phone": "081 2233 44-55",
+          "address": "Sudirman",
+          "city": "Jakarta",
+          "postal_code": "12190",
+          "country_code": "IDN"
+        },
+        "shipping_address": {
+          "first_name": "BUDI",
+          "last_name": "UTOMO",
+          "email": "test@midtrans.com",
+          "phone": "0 8128-75 7-9338",
+          "address": "Sudirman",
+          "city": "Jakarta",
+          "postal_code": "12190",
+          "country_code": "IDN"
+        }
+      }
+  }
+?>
