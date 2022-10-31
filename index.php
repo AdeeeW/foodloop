e3ee01096caa906ec331184ef96855d3b1c4d08e
AdeeeW <adewijayanto2@gmail.com>
2022-10-31 11:55:24 +0700

    update file index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Set your server key (Note: Server key for sandbox and production mode are different)
-$server_key = "SB-Mid-server-tzzEHOP-lpV8zOELcgVxLhKz";
+$server_key = 'SB-Mid-server-tzzEHOP-lpV8zOELcgVxLhKz';
 // Set true for production, set false for sandbox
 $is_production = false;
 
